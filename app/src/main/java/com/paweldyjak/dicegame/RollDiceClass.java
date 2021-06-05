d19da71f1ba257a75af658862f2b9940dde26a3a
Pawel <pawel.dyjak@poczta.onet.pl>
2021-06-05 01:05:41 +0100

    Testing text view for displaying dices

--- app/src/main/java/com/paweldyjak/dicegame/RollDiceClass.java
+++ app/src/main/java/com/paweldyjak/dicegame/RollDiceClass.java
@@ -2,40 +2,46 @@ package com.paweldyjak.dicegame;
 
 import android.app.Activity;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
 import java.util.Random;
 
 public class RollDiceClass {
     public Activity activity;
     private Button rollDiceButton;
+    private TextView displayNumbers;
     String[] rolledValues = new String[6];
 
-    RollDiceClass(Activity activity){
+    RollDiceClass(Activity activity) {
         this.activity = activity;
         setRollDiceButton();
     }
 
-    public void setRollDiceButton(){
-        rollDiceButton = (Button) this.activity.findViewById(R.id.roll_dice_button);
+    public void setRollDiceButton() {
+        rollDiceButton = activity.findViewById(R.id.roll_dice_button);
         rollDiceButton.setOnClickListener(v -> {
-            rollSixDices();
-            Toast.makeText(activity, rolledValues[0]+rolledValues[1]+rolledValues[2]+rolledValues[3]+rolledValues[4]+rolledValues[5], Toast.LENGTH_LONG).show();
-
+            rollDices();
+            displayNumbers();
         });
 
     }
 
-    public int[] rollSixDices(){
+    public int[] rollDices() {
         int[] values = new int[6];
         Random randomValue = new Random();
 
-        for (int x = 0; x<6;x++){
-            int value = randomValue.nextInt(6-1+1)+1;
+        for (int x = 0; x < 6; x++) {
+            int value = randomValue.nextInt(6 - 1 + 1) + 1;
             rolledValues[x] = String.valueOf(value);
         }
-return values;
+
+        return values;
+    }
+
+    public void displayNumbers() {
+        displayNumbers = activity.findViewById(R.id.numbers_display);
+        displayNumbers.setText("Siemano");
     }
 
 }
