f76015a3058de4300b8932f231e4eb135f514895
Pawel <pawel.dyjak@poczta.onet.pl>
2021-06-07 23:45:37 +0100

    Setting table layout for score table

--- app/src/main/java/com/paweldyjak/dicegame/MainActivity.java
+++ app/src/main/java/com/paweldyjak/dicegame/MainActivity.java
@@ -3,15 +3,18 @@ package com.paweldyjak.dicegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //hide status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //hides title bar
         getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
         RollDiceClass rollDiceClass = new RollDiceClass(this);
         rollDiceClass.setRollDiceButton();
         rollDiceClass.setDicesSlots();
