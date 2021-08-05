2448303fa911c08e48c5699406eb104229f58ee6
Pawel <pawel.dyjak@poczta.onet.pl>
2021-08-05 22:42:09 +0100

    adding menu interface

--- app/src/main/java/com/paweldyjak/dicegame/Fragments/TitleScreen.java
+++ app/src/main/java/com/paweldyjak/dicegame/Fragments/TitleScreen.java
@@ -5,14 +5,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import androidx.fragment.app.Fragment;
+
 import com.paweldyjak.dicegame.*;
 
 
 public class TitleScreen extends Fragment {
     private final MainActivity mainActivity;
     private final PlayerNamesInputScreen playerNamesInputScreen;
-    private Button startGameButton;
+    private Button multiplayerButton;
+    private Button exitButton;
+    private Button twoPlayersButton;
+    private View playersNumberLayout;
 
     public TitleScreen(MainActivity mainActivity, PlayerNamesInputScreen playerNamesInputScreen) {
         this.mainActivity = mainActivity;
@@ -24,15 +29,22 @@ public class TitleScreen extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.title_screen_fragment, container, false);
-        startGameButton = view.findViewById(R.id.twoPlayersButton);
-        setTwoPlayersButton();
+        multiplayerButton = view.findViewById(R.id.multiplayer_button);
+        exitButton = view.findViewById(R.id.exit_button);
+        twoPlayersButton = view.findViewById(R.id.two_players_button);
+        playersNumberLayout = view.findViewById(R.id.players_number_layout);
+        setButtons();
+
         return view;
     }
 
-    public void setTwoPlayersButton() {
-        startGameButton.setOnClickListener(v -> mainActivity.replaceFragment(R.id.fragment_layout, playerNamesInputScreen));
-
-
+    public void setButtons() {
+        multiplayerButton.setOnClickListener(v -> {
+            playersNumberLayout.setVisibility(View.VISIBLE);
+            multiplayerButton.setEnabled(false);
+        });
+        twoPlayersButton.setOnClickListener(v -> mainActivity.replaceFragment(R.id.fragment_layout, playerNamesInputScreen));
+        exitButton.setOnClickListener(v -> System.exit(0));
 
 
     }
