5807ba407a51784d716f756a6ccbe65e81200e30
Pawel <pawel.dyjak@poczta.onet.pl>
2021-09-14 23:15:40 +0100

    Multiplayer queue class refactored

--- app/src/main/java/com/paweldyjak/dicegame/Activities/MultiplayerQueueActivity.java
+++ app/src/main/java/com/paweldyjak/dicegame/Activities/MultiplayerQueueActivity.java
@@ -1,7 +1,6 @@
 package com.paweldyjak.dicegame.Activities;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import android.content.Intent;
@@ -13,11 +12,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.MutableData;
-import com.google.firebase.database.Transaction;
 import com.google.firebase.database.ValueEventListener;
 import com.paweldyjak.dicegame.*;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -33,8 +29,6 @@ public class MultiplayerQueueActivity extends AppCompatActivity {
     private String opponentName;
     private String playerName;
     private String[] playersNames;
-    private DatabaseReference multiplayerRoomReference;
-    private DatabaseReference playerNameReference;
     private int playerRanking;
     private int opponentRanking;
 
@@ -50,46 +44,48 @@ public class MultiplayerQueueActivity extends AppCompatActivity {
         opponentFoundTextView = findViewById(R.id.user_found_textView);
         multiplayerQueueReference = FirebaseDatabase.getInstance().getReference().child("multiplayerQueue");
         playerUid = firebaseAuth.getUid();
-        playerNameReference = FirebaseDatabase.getInstance().getReference().child("users").child(playerUid).child("name");
         playersNames = new String[2];
         joinMultiplayerQueue();
+        updatePlayersInQueueNumber();
+        setPlayerName();
         lookForOpponentPlayer();
     }
 
-
     public void joinMultiplayerQueue() {
-        //join multiplayer queue
+        multiplayerQueueReference.child("playersUid").child(playerUid).setValue(0);
+    }
+
+    public void updatePlayersInQueueNumber() {
+        multiplayerQueueReference.child("playersInQueue").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (opponentName == null) {
+                    playersInQueue = snapshot.getValue(Integer.class);
+                    playersInQueue += 1;
+                    multiplayerQueueReference.child("playersInQueue").setValue(playersInQueue);
+                } else {
+                    playersInQueue = snapshot.getValue(Integer.class);
+                    playersInQueue -= 1;
+                    multiplayerQueueReference.child("playersInQueue").setValue(playersInQueue);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    public void setPlayerName() {
+        DatabaseReference playerNameReference = FirebaseDatabase.getInstance().getReference().child("users").child(playerUid).child("name");
         playerNameReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 playerName = snapshot.getValue(String.class);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
-        //update players in queue number
-        multiplayerQueueReference.child("playersInQueue").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                playersInQueue = snapshot.getValue(Integer.class);
-                playersInQueue += 1;
-                multiplayerQueueReference.child("playersInQueue").setValue(playersInQueue);
-
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        //create database entry for players in queue
-        multiplayerQueueReference.child("playersUid").child(playerUid).setValue(0);
-
-
     }
 
     public void lookForOpponentPlayer() {
@@ -99,78 +95,54 @@ public class MultiplayerQueueActivity extends AppCompatActivity {
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     if (!dataSnapshot.getKey().equals(playerUid)) {
                         opponentUid = dataSnapshot.getKey();
-                        DatabaseReference opponentNameReference = FirebaseDatabase.getInstance().getReference().child("users").child(opponentUid).child("name");
-                        multiplayerRoomReference = FirebaseDatabase.getInstance().getReference().child("users").child(opponentUid).child("multiplayerRoom").child(playerUid);
-                        multiplayerRoomReference.setValue(0);
-                        opponentNameReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                opponentName = snapshot.getValue(String.class);
-                                opponentName = snapshot.getValue(String.class);
-                                opponentFoundTextView.setText(opponentName);
-                                multiplayerQueueReference.child("playersUid").removeValue();
-                                setPlayersOrder();
-
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-
-                            }
-                        });
-                        multiplayerQueueReference.child("playersInQueue").runTransaction(new Transaction.Handler() {
-                            @NonNull
-                            @Override
-                            public Transaction.Result doTransaction(@NonNull MutableData currentData) {
-                                currentData.setValue((Long) currentData.getValue() - 1);
-                                return Transaction.success(currentData);
-                            }
-
-                            @Override
-                            public void onComplete(@Nullable @org.jetbrains.annotations.Nullable DatabaseError error, boolean committed, @Nullable @org.jetbrains.annotations.Nullable DataSnapshot currentData) {
-                            }
-                        });
-
-
+                        createMultiplayerRoom();
+                        getOpponentName();
+                        updatePlayersInQueueNumber();
+                        setPlayersOrder();
                     }
-
                 }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+        if (opponentName != null) {
+            Executor executor = ContextCompat.getMainExecutor(this);
+            executor.execute(this::lookForOpponentPlayer);
+        }
+    }
 
+    public void createMultiplayerRoom() {
+        DatabaseReference multiplayerRoomReference = FirebaseDatabase.getInstance().getReference().child("users").child(opponentUid).child("multiplayerRoom").child(playerUid);
+        multiplayerRoomReference.setValue(0);
+    }
 
+    public void getOpponentName() {
+        DatabaseReference opponentNameReference = FirebaseDatabase.getInstance().getReference().child("users").child(opponentUid).child("name");
+        opponentNameReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                opponentName = snapshot.getValue(String.class);
+                opponentFoundTextView.setText(opponentName);
+                multiplayerQueueReference.child("playersUid").removeValue();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
-
         });
-        if (!opponentFoundTextView.getText().equals(getResources().getString(R.string.opponent_search))) {
-            Executor executor = ContextCompat.getMainExecutor(this);
-            executor.execute(this::lookForOpponentPlayer);
-
-        }
-
-
     }
-
     @Override
     public void onBackPressed() {
-
     }
-
 
     public void startMultiplayerGame(String[] playersNames) {
         Executor executor = ContextCompat.getMainExecutor(this);
         executor.execute(() -> {
             try {
                 Thread.sleep(3000);
-
-
             } catch (Exception ignored) {
-
             }
-
         });
         GameBoardActivity gameBoardActivity = new GameBoardActivity();
         Intent intent = new Intent(this, gameBoardActivity.getClass());
@@ -179,7 +151,7 @@ public class MultiplayerQueueActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void setPlayersOrder(){
+    public void setPlayersOrder() {
         DatabaseReference playerRankingReference = FirebaseDatabase.getInstance().getReference().child("users").child(playerUid).child("ranking");
         DatabaseReference opponentRankingReference = FirebaseDatabase.getInstance().getReference().child("users").child(opponentUid).child("ranking");
         playerRankingReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -187,42 +159,33 @@ public class MultiplayerQueueActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 playerRanking = snapshot.getValue(Integer.class);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
         opponentRankingReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 opponentRanking = snapshot.getValue(Integer.class);
-                if(playerRanking>opponentRanking){
+                if (playerRanking > opponentRanking) {
                     playersNames[0] = playerName;
                     playersNames[1] = opponentName;
-                } else if(opponentRanking>playerRanking){
+                } else if (opponentRanking > playerRanking) {
                     playersNames[0] = opponentName;
                     playersNames[1] = playerName;
-                } else{
+                } else {
                     List<String> players = new ArrayList<>();
                     players.add(playerName);
                     players.add(opponentName);
                     Collections.sort(players);
                     playersNames[0] = players.get(0);
                     playersNames[1] = players.get(1);
-
                 }
                 startMultiplayerGame(playersNames);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
-
-
-
     }
-
 }
